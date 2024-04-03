60161aa85a53e290c8e9d9ef91fb8cc9b1485fde
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2024-04-03 22:01:33 +0200

    Added extra validation to DrTracker

--- DrTracker.lua
+++ DrTracker.lua
@@ -13,22 +13,29 @@ function DrTracker:AddDr(unitGUID, category, ccDuration)
 
         self.unitDRs[unitGUID] = template
     end
+
     local DRs = self.unitDRs[unitGUID]
     local drDuration = DrList:GetResetTime(category)
-
-    local currentDrLevel = DRs[category].level
-    if currentDrLevel + 1 >= 2 then currentDrLevel = 1 end
     local expirationTime = time() + drDuration + ccDuration
-
-    DRs[category].level = currentDrLevel + 1
+    
     DRs[category].expirationTime = expirationTime
+    DRs[category].level = DRs[category].level + 1
+    if DRs[category].level > 2 then DRs[category].level = 2 end
 end
 
 function DrTracker:GetDrInfo(unitGUID, spellId)
     local DRs = self.unitDRs[unitGUID]
+    -- No dr entry means we don't know anything about target's dr, thus assume none
+    if DRs == nil then return 0, nil end
+
     local category = DrList:GetCategoryBySpellID(spellId)
 
     local level = DRs[category].level
     local remainingDrTime = DRs[category].expirationTime - time()
+    if remainingDrTime < 0 then
+        remainingDrTime = 0
+        level = 0
+        DRs[category].level = 0
+    end
     return level, remainingDrTime
 end

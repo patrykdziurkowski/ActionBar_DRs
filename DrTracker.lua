9d90b518006351e938f70716fe697bef535a7a5d
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2024-04-05 23:50:06 +0200

    Split some of the logic of DrTracker into a smaller child object

--- DrTracker.lua
+++ DrTracker.lua
@@ -1,42 +1,67 @@
 local DrList = LibStub:GetLibrary("DRList-1.0")
 
+-- UNIT DRs
+local UnitDRs = {}
+
+function UnitDRs:New(o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+
+    local categories = DrList:GetCategories()
+    for category, _ in pairs(categories) do
+        o[category] = { level = 0, expirationTime = 0 }
+    end
+
+    return o
+end
+
+function UnitDRs:Refresh(category, expirationTime)
+    self:Update(category)
+
+    self[category].expirationTime = expirationTime
+    if self[category].level < 2 then
+        self[category].level = self[category].level + 1
+    end
+end
+
+function UnitDRs:Update(category)
+    local remainingDrTime = self[category].expirationTime - time()
+    if remainingDrTime < 0 then
+        self[category].level = 0
+        self[category].expirationTime = 0
+    end
+end
+
+
+-- DR TRACKER
 DrTracker = {}
 DrTracker.unitDRs = {}
 
+
 function DrTracker:AddDr(unitGUID, category, ccDuration)
     if self.unitDRs[unitGUID] == nil then
-        local categories = DrList:GetCategories()
-        local template =  {}
-        for category, _ in pairs(categories) do
-            template[category] = { level = 0, expirationTime = 0}
-        end
-
-        self.unitDRs[unitGUID] = template
+        self.unitDRs[unitGUID] = UnitDRs:New()
     end
 
-    local DRs = self.unitDRs[unitGUID]
+    local drs = self.unitDRs[unitGUID]
     local drDuration = DrList:GetResetTime(category)
     local expirationTime = time() + drDuration + ccDuration
     
-    DRs[category].expirationTime = expirationTime
-    DRs[category].level = DRs[category].level + 1
-    if DRs[category].level > 2 then DRs[category].level = 2 end
+    drs:Refresh(category, expirationTime)
 end
 
 function DrTracker:GetDrInfo(unitGUID, spellId)
-    local DRs = self.unitDRs[unitGUID]
     -- No dr entry means we don't know anything about target's dr, thus assume none
-    if DRs == nil then return 0, nil end
+    if self.unitDRs[unitGUID] == nil then return 0, nil end
 
+    local drs = self.unitDRs[unitGUID]
     local category = DrList:GetCategoryBySpellID(spellId)
 
-    local level = DRs[category].level
-    local remainingDrTime = DRs[category].expirationTime - time()
-    if remainingDrTime < 0 then
-        remainingDrTime = 0
-        level = 0
-        DRs[category].level = 0
-        DRs[category].expirationTime = 0
-    end
+    drs:Update(category)
+
+    local level = drs[category].level
+    local remainingDrTime = drs[category].expirationTime - time()
+    if remainingDrTime < 0 then remainingDrTime = 0 end
     return level, remainingDrTime
 end

8c3a92a0a9fb78da8371f448b8a3b208ed4564e0
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2024-04-06 21:11:51 +0200

    Changed the code not to be as nested using guard clauses

--- Main.lua
+++ Main.lua
@@ -23,18 +23,17 @@ end)
 local f = CreateFrame("Frame")
 f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 f:SetScript("OnEvent", function(self, event)
-    if event == "COMBAT_LOG_EVENT_UNFILTERED" then
-        local _, subevent, _, casterGUID, _, _, _, targetGUID, _, _, _, spellId = CombatLogGetCurrentEventInfo()
-        if subevent == "SPELL_CAST_SUCCESS" then
-            local category = DrList:GetCategoryBySpellID(spellId)
-            -- if category is nil then it's not a CC
-            if category == nil then return end
+    if event ~= "COMBAT_LOG_EVENT_UNFILTERED" then return end
+    local _, subevent, _, casterGUID, _, _, _, targetGUID, _, _, _, spellId = CombatLogGetCurrentEventInfo()
+    if subevent ~= "SPELL_CAST_SUCCESS" then return end
 
-            local ccDuration = 4
-            --check if target not immune, npc, etc.
-            --different dr durations for knockback etc.
-            DrTracker:AddDr(targetGUID, category, ccDuration)
-            f2:GetScript("OnEvent")(f2, "PLAYER_TARGET_CHANGED");
-        end
-    end
+    -- if category is nil then it's not a CC so we skip
+    local category = DrList:GetCategoryBySpellID(spellId) 
+    if category == nil then return end
+
+    --check if target not immune, npc, etc.
+    --different dr durations for knockback etc.
+    local ccDuration = 4
+    DrTracker:AddDr(targetGUID, category, ccDuration)
+    f2:GetScript("OnEvent")(f2, "PLAYER_TARGET_CHANGED");
 end)

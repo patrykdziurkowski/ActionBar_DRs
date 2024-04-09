72b10d834b1dfc1701a5e9373165e8670caaa22a
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2024-04-09 18:05:22 +0200

    Refactoring to improve readability

--- Main.lua
+++ Main.lua
@@ -1,13 +1,13 @@
 local DrList = LibStub:GetLibrary("DRList-1.0")
 local DrTracker = DrTracker
 local FrameManager = FrameManager
+local AddOn = {}
 
-
-local buttons = {}
+AddOn.buttons = {}
 C_Timer.After(2, function()
     for i = 1, 360 do
         if _G["BT4Button"..i] ~= nil then
-            buttons[i] = _G["BT4Button"..i]
+            AddOn.buttons[i] = _G["BT4Button"..i]
         end
     end
 end)
@@ -16,26 +16,7 @@ end)
 local f2 = CreateFrame("Frame")
 f2:RegisterEvent("PLAYER_TARGET_CHANGED")
 f2:SetScript("OnEvent", function(self, event)
-    local targetGUID = UnitGUID("target")
-
-    if targetGUID == nil then
-        for i, button in pairs(buttons) do
-            FrameManager:HideBorder(button)
-        end
-        return
-    end
-    
-    for i, button in pairs(buttons) do
-        local type, spellId = GetActionInfo(button.id)
-        if type == "spell" then
-            local level, appliedTime, remainingDrTime = DrTracker:GetDrInfo(targetGUID, spellId)
-            if level == 0 then 
-                FrameManager:HideBorder(button)
-            else
-                FrameManager:ShowBorder(button, level, appliedTime, GetTime() + remainingDrTime)
-            end
-        end
-    end
+    AddOn:UpdateFrames()
 end)
 
 
@@ -55,21 +36,46 @@ f:SetScript("OnEvent", function(self, event)
     local spellName = GetSpellInfo(spellId)
     local unit = UnitTokenFromGUID(targetGUID)
     
-    if unit ~= nil then
-        -- small delay because apparently this code runs before the debuff is available via AuraUtils
-        RunNextFrame(function()
-            local _, _, _, _, duration = AuraUtil.FindAuraByName(spellName, unit, "HARMFUL")
-            -- nil duration means someone's immuning the CC so dont extend it
-            if duration == nil then return end
-            DrTracker:AddDr(targetGUID, category, duration)
-            f2:GetScript("OnEvent")(f2, "PLAYER_TARGET_CHANGED");
-        end)
-    else
-        -- default cc duration if we fail figuring it out exactly
+    -- default estimated cc duration if we fail figuring it out exactly
+    if unit == nil then
         local level = DrTracker:GetDrInfo(targetGUID, spellId)
         local diminishFactor = DrList:GetNextDR(level, category)
         local ccDuration = 6 * diminishFactor
         DrTracker:AddDr(targetGUID, category, ccDuration)
-        f2:GetScript("OnEvent")(f2, "PLAYER_TARGET_CHANGED");
+        AddOn:UpdateFrames()
+        return
     end
+
+    -- small delay because apparently this code runs before the debuff is available via AuraUtils
+    RunNextFrame(function()
+        local _, _, _, _, duration = AuraUtil.FindAuraByName(spellName, unit, "HARMFUL")
+        -- nil duration means someone's immuning the CC so dont extend it
+        if duration == nil then return end
+        DrTracker:AddDr(targetGUID, category, duration)
+        AddOn:UpdateFrames()
+    end)
+       
 end)
+
+function AddOn:UpdateFrames()
+    local targetGUID = UnitGUID("target")
+
+    if targetGUID == nil then
+        for i, button in pairs(AddOn.buttons) do
+            FrameManager:HideBorder(button)
+        end
+        return
+    end
+    
+    for i, button in pairs(AddOn.buttons) do
+        local type, spellId = GetActionInfo(button.id)
+        if type == "spell" then
+            local level, appliedTime, remainingDrTime = DrTracker:GetDrInfo(targetGUID, spellId)
+            if level == 0 then 
+                FrameManager:HideBorder(button)
+            else
+                FrameManager:ShowBorder(button, level, appliedTime, GetTime() + remainingDrTime)
+            end
+        end
+    end
+end

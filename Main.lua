7404d4c1f6f86988f90fc920e922e0f136d09a78
PatrykDziurkowski <patryk.j.dziurkowski@gmail.com>
2024-04-12 19:33:44 +0200

    Split up Main.lua into smaller functions

--- Main.lua
+++ Main.lua
@@ -1,8 +1,10 @@
 local DrList = LibStub:GetLibrary("DRList-1.0")
+-- Module that tracks Diminishing Returns
 local DrTracker = DrTracker
+-- Module that creates the DR frames around buttons
 local FrameManager = FrameManager
-local AddOn = {}
 
+local AddOn = {}
 AddOn.buttons = {}
 C_Timer.After(2, function()
     for i = 1, 360 do
@@ -15,12 +17,14 @@ end)
 
 
 
+--[[
+    EVENTS
+]]--
 local f2 = CreateFrame("Frame")
 f2:RegisterEvent("PLAYER_TARGET_CHANGED")
 f2:SetScript("OnEvent", function(self, event)
     AddOn:UpdateFrames()
 end)
-
 
 local f = CreateFrame("Frame")
 f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
@@ -33,16 +37,22 @@ f:SetScript("OnEvent", function(self, event)
     local category = DrList:GetCategoryBySpellID(spellId) 
     if category == nil then return end
 
+    AddOn:CcCasted(targetGUID, spellId, category)
+end)
+
+
+
+
+
+--[[
+    ADDON FUNCTIONS
+]]--
+
+function AddOn:CcCasted(targetGUID, spellId, category)
     local spellName = GetSpellInfo(spellId)
     local unit = UnitTokenFromGUID(targetGUID)
-
-    -- quest boss mobs (i.e. the ones with an exclamation mark on their nameplate, not just any mob needed for a quest) tend to receive full DRs
-    -- players and their pets receive full DRs
-    -- other pve mobs either immune the CCs or they receive DRs only from a subset of their categories
-    local hasDiminishingReturns = UnitIsPlayer(unit) or UnitIsOtherPlayersPet(unit) or UnitIsQuestBoss(unit) or DrList:IsPvECategory(category)
-    if not hasDiminishingReturns then return end
     
-    -- couldn't find unit anywhere in nameplates, party, pets, target, raid, etc.
+    -- nil unit means we couldn't find unit anywhere in nameplates, party, pets, target, raid, etc., see below:
     -- https://wowpedia.fandom.com/wiki/API_UnitTokenFromGUID
     if unit == nil then
         -- default estimated cc duration if we fail figuring it out exactly
@@ -54,6 +64,12 @@ f:SetScript("OnEvent", function(self, event)
         return
     end
 
+    -- quest boss mobs (i.e. the ones with an exclamation mark on their nameplate, not just any mob needed for a quest) tend to receive all DR categories
+    -- players and their pets receive all DR categories
+    -- other pve mobs either immune the CCs or they receive DRs only from a subset of categories
+    local hasDiminishingReturns = UnitIsPlayer(unit) or UnitIsOtherPlayersPet(unit) or UnitIsQuestBoss(unit) or DrList:IsPvECategory(category)
+    if not hasDiminishingReturns then return end
+
     -- small delay because apparently this code runs before the debuff is available via AuraUtils
     RunNextFrame(function()
         local _, _, _, _, duration = AuraUtil.FindAuraByName(spellName, unit, "HARMFUL")
@@ -62,27 +78,24 @@ f:SetScript("OnEvent", function(self, event)
         DrTracker:AddDr(targetGUID, category, duration)
         AddOn:UpdateFrames()
     end)
-end)
+end
 
 function AddOn:UpdateFrames()
     local targetGUID = UnitGUID("target")
 
     if targetGUID == nil then self:HideDrIndicators() return end
-    
     self:DisplayDrIndicators(targetGUID)
 end
 
 function AddOn:DisplayDrIndicators(targetGUID)
     for _, button in pairs(AddOn.buttons) do
         local type, spellId = GetActionInfo(button.id)
-        if type == "spell" then
-            local level, appliedTime, remainingDrTime = DrTracker:GetDrInfo(targetGUID, spellId)
-            if level == 0 then 
-                FrameManager:HideBorders(button)
-            else
-                FrameManager:ShowBorders(button, level, appliedTime, GetTime() + remainingDrTime)
-            end
-        end
+        if type ~= "spell" then return end
+
+        local level, appliedTime, remainingTime = DrTracker:GetDrInfo(targetGUID, spellId)
+        if level == 0 then FrameManager:HideBorders(button) return end
+
+        FrameManager:ShowBorders(button, level, appliedTime, GetTime() + remainingTime)
     end
 end
 
